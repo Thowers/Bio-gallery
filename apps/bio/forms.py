44fadd77058c99ea780fdc3b9fb356db718cccee
Nicolas Torres <nicolasparratorres@gmail.com>
2025-05-01 19:33:18 -0500

    Notificaciones de error registro.

--- apps/bio/forms.py
+++ apps/bio/forms.py
@@ -20,6 +20,18 @@ class RegistroForm(forms.ModelForm):
             'email': forms.EmailInput(attrs={'class': 'form-control','id': 'email','required': True}),
         }
 
+    def clean_usuario(self):
+        usuario = self.cleaned_data['usuario']
+        if Registro.objects.filter(usuario__iexact=usuario).exists():
+            raise forms.ValidationError("Este nombre de usuario ya está en uso.")
+        return usuario
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if Registro.objects.filter(email__iexact=email).exists():
+            raise forms.ValidationError("Este correo ya está registrado.")
+        return email
+
     def save(self, commit=True):
         registro = super().save(commit=False)
         registro.password = make_password(self.cleaned_data['password'])
