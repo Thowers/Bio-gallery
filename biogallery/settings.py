82da80df90a7de3636a722004fb5bc7e2f3ab19f
Nicolas Torres <nicolasparratorres@gmail.com>
2025-05-30 00:35:08 -0500

    settings5

--- biogallery/settings.py
+++ biogallery/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'biogallery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "biogallery/templates")],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
