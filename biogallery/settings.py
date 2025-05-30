c9467724cd492b535490a1dd78ac51f2eb844d61
Nicolas Torres <nicolasparratorres@gmail.com>
2025-05-30 00:29:53 -0500

    settings4

--- biogallery/settings.py
+++ biogallery/settings.py
@@ -116,7 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
